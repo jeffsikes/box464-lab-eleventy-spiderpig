9742ffee5efd93b34aea58d8d7972820ddb88b97
Jeff Sikes <jeffsikes@users.noreply.github.com>
2023-02-21 23:30:47 -0600

    added opengraph images

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const { DateTime } = require('luxon');
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/style.css");
+    eleventyConfig.addPassthroughCopy("./src/peter_porker.png");
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromISO(dateObj).toLocaleString(DateTime.DATE_MED);
     });
